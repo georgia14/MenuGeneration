2012c2d23c7bd142d23ac2abf7a5e45db740d03c
Georgia <georgia.karapostoli@gmail.com>
2014-03-13 11:11:13 +0100

    fixed bug with double/quad triggers

--- bin/makeGraph.sh
+++ bin/makeGraph.sh
@@ -22,8 +22,8 @@ rm -rf $_macro
 
 echo "void test() {" >> $_macro
 
-cat $_input | grep "Total L1 Rate (with overlaps)" | awk '{print $7}' > $_file &
-cat $_file
+cat $_input | grep "Total L1 Rate (with overlaps)" | awk '{print $7}' > $_file 
+#cat $_file
 
 # x Array : bandwidth values
 _n=$(wc -l < $_file)
@@ -37,8 +37,8 @@ echo "Double_t x["$_n"]={"$_xarray"};" >> $_macro
 rm -rf $_file
 
 #bandwidth error array
-cat $_input | grep "Total L1 Rate (with overlaps)" | awk '{print $9}' > $_file &
-cat $_file
+cat $_input | grep "Total L1 Rate (with overlaps)" | awk '{print $9}' > $_file 
+#cat $_file
 
 _earray=( `cat $_file` )
 _xearray=$(IFS=, ; echo "${_earray[*]}")
@@ -50,50 +50,115 @@ echo "Double_t ex["$_n"]={"$_xearray"};" >> $_macro
 # Trigger name array
 _triggerArray=( L1_SingleEG L1_SingleIsoEG L1_SingleMu L1_SingleIsoMu L1_SingleTau L1_SingleIsoTau L1_isoEG_EG L1_isoMu_Mu L1_isoTau_Tau L1_isoEG_Mu L1_isoMu_EG L1_isoEG_Tau L1_isoMu_Tau L1_SingleJetC L1_DoubleJet L1_QuadJetC L1_SingleIsoEG_CJet L1_SingleMu_CJet L1_SingleIsoEG_HTM L1_SingleMu_HTM L1_HTM L1_HTT )
 
-i=0; iT=-1
+_triggerArray1=( L1_SingleEG L1_SingleIsoEG L1_SingleMu L1_SingleIsoMu L1_SingleTau L1_SingleIsoTau L1_SingleJetC L1_HTM L1_HTT )
+_triggerArray2=( L1_isoEG_EG L1_isoMu_Mu L1_isoTau_Tau L1_isoEG_Mu L1_isoMu_EG L1_isoEG_Tau L1_isoMu_Tau L1_DoubleJet L1_SingleIsoEG_CJet L1_SingleMu_CJet L1_SingleIsoEG_HTM L1_SingleMu_HTM )
+_triggerArray4=( L1_QuadJetC )
+
+iT=-1; obj=-1
+
+_testit=0
+
+for _trigger in "${_triggerArray1[@]}"
+do
+  if [ $_trigger == $2 ]; then obj=1; 
+      echo "This is a Single object trigger"
+      break; fi
+done
+for _trigger in "${_triggerArray2[@]}"
+do
+  if [ $_trigger == $2 ]; then obj=2; 
+      echo "This is a double object trigger"
+      break; fi
+done
+for _trigger in "${_triggerArray4[@]}"
+do
+  if [ $_trigger == $2 ]; then obj=4; 
+      echo "This is a quad object trigger"
+      break; fi
+done
+
+if [ $obj -lt $_testit ] 
+    then echo "Could not verify Trigger. Please check trigger name!"; exit
+fi
+
+i=0
 for _trigger in "${_triggerArray[@]}"
 do
 #  echo $_trigger
   if [ $_trigger == $2 ]; then iT=$i; break; fi
   let i=${i}+1
 done
-
-_testit=0
 if [ $iT -lt $_testit ] 
     then echo "Please insert Trigger name!!!"; exit
 fi
 
 echo ${_triggerArray[$iT]}
+
+
+# Num object array
+_obj=( 1 2 4 ) 
+
+if [ $obj = ${_obj[0]} ]; then
+
 # y arrays: threshold values
-rm -rf $_file
-cat $_input | grep "${_triggerArray[$iT]} " | awk '{print $2}' > $_file &
-cat $_file
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $2 }' > $_file 
+    _yarray0=( `cat $_file` )
+    
+# threshold error low
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $7 }' > $_file 
+    _yelarray0=( `cat $_file` )
+    
+# threshold error high
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $8 }' > $_file 
+    _yeharray0=( `cat $_file` )
 
-_yarray1=( `cat $_file` )
-_yarray=$(IFS=, ; echo "${_yarray1[*]}")
+elif [ $obj = ${_obj[1]} ]; then
 
+# y arrays: threshold values
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $2 }' > $_file 
+    _yarray0=( `cat $_file` )
+    
+# threshold error low
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $8 }' > $_file 
+    _yelarray0=( `cat $_file` )
+    
+# threshold error high
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $9 }' > $_file 
+    _yeharray0=( `cat $_file` )
+
+elif [ $obj = ${_obj[2]} ]; then
+
+# y arrays: threshold values
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $2 }' > $_file 
+    _yarray0=( `cat $_file` )
+    
+# threshold error low
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $10 }' > $_file 
+    _yelarray0=( `cat $_file` )
+    
+# threshold error high
+    rm -rf $_file
+    cat $_input | grep "${_triggerArray[$iT]} " | awk '{ print $11 }' > $_file 
+    _yeharray0=( `cat $_file` )
+fi
+
+_yarray=$(IFS=, ; echo "${_yarray0[*]}")
 echo "y="$_yarray
 echo "Double_t y["$_n"]={"$_yarray"};" >> $_macro
 
-# threshold error low
-rm -rf $_file
-cat $_input | grep "${_triggerArray[$iT]} " | awk '{print $7}' > $_file &
-cat $_file
-
-_yelarray0=( `cat $_file` )
 _yelarray=$(IFS=, ; echo "${_yelarray0[*]}")
-
 echo "eylow="$_yelarray
 echo "Double_t eyl["$_n"]={"$_yelarray"};" >> $_macro
 
-# threshold error high
-rm -rf $_file
-cat $_input | grep "${_triggerArray[$iT]} " | awk '{print $8}' > $_file &
-cat $_file
-
-_yeharray0=( `cat $_file` )
 _yeharray=$(IFS=, ; echo "${_yeharray0[*]}")
-
 echo "eyhigh="$_yeharray
 echo "Double_t eyh["$_n"]={"$_yeharray"};" >> $_macro
 
@@ -105,7 +170,7 @@ echo "TGraphAsymmErrors *g= new TGraphAsymmErrors("$_n",x,y,ex,ex,eyl,eyh);" >> 
 echo "TCanvas *c1=new TCanvas(\"can1\",\"\");" >> $_macro
 echo "gPad->SetGridx(); gPad->SetGridy();" >> $_macro
 echo "g->Draw(\"AP4\");" >> $_macro
-#echo "g->GetYaxis()->SetRangeUser(5.,60.);" >> $_macro
+echo "g->GetYaxis()->SetRangeUser(0.,500.);" >> $_macro
 echo "g->SetTitle(\";Bandwidth (kHz);Threshold (GeV)\");" >> $_macro
 echo "g->SetFillColor(3);" >> $_macro
 echo "}" >> $_macro
